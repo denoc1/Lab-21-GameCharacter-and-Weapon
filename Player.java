95977d1ca6dc31b3450957eaf833c5ee541a4799
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-12-12 03:48:41 +0000

    with comments

--- Player.java
+++ Player.java
@@ -5,36 +5,72 @@ private Weapon equippedWeapon;
 
 
 // Constructor
+/**Constructor to create a new Player with a Weapon
+        * Precondions - name != null
+        *             - health >= 0
+        *             - weapon != null
+        * Postconditions - Creates a new Player with defensive copy of Weapon
 public Player(String name, int health, Weapon weapon) {
 // TODO: implement defensive copy of weapon
 }
 
 
 // Copy constructor
+/**Constructor to creates a  new Player identical to other
+        * Precondions - other != null
+        * Postconditions -A new Player object exists with copied name, health, and weapon (deep copy)
 public Player(Player other) {
 // TODO: implement deep copy
 }
 
 
 // Getters
+/** Returns the name of the Player
+ *  Postcondition - Returns the name of type String
+ *  */
 public String getName() { 
     /* TODO */ 
     }
+
+/** Returns the Player's health
+*   Postcondition - Returns the health of type integer
+*/
 public int getHealth() { 
     /* TODO */
      }
+
+/** Returns a copy of Player's Weapon
+*  Postcondition - Returns a new Weapon object identical to the equipped weapon
+*/
 public Weapon getWeapon() { 
     /* TODO: return a copy */ 
      }
 
 
 // Methods
+
+/** Equip a new weapon
+* Precondition - w != null
+*  Postcondition - Player now has a copy of weapon w as their equippedWeapon
+*/
 public void equipWeapon(Weapon w) { 
     /* TODO: defensive copy */ 
     }
+
+/** Reduces the Player's health by dmg
+* Precondition - dmg >= 0
+*  Postcondition - health reduced by dmg, minimum 0
+*/
 public void takeDamage(int dmg) { 
     /* TODO */ 
     }
+
+
+/** Attacks another Player using this Player’s weapon
+* Precondition - other != null, 
+*              - weapon not broken
+*  Postcondition - other.health reduced by weapon damage, this Player’s weapon durability decreased by 1
+*/    
 public void attack(Player other) { 
     /* TODO */ 
     }
--- Weapon.java
+++ Weapon.java
@@ -4,34 +4,62 @@ public class Weapon {
     private int durability;
 
     // Constructor
+    /**Constructor to create a new Weapon
+        * Precondions - name is non-null
+        *             - damage >= 0
+        *             - durability >= 0
+        * Postconditions - creates a new Weaon with given attributes
+    */ 
     public Weapon(String name, int damage, int durability) {
     // TODO: implement
     }
 
 
     // Copy constructor
+        /**Constructor to create a Weapon identical to other
+        *   Precondions - other !-null
+        *   Postconditions - A new Weapon object exists with the same name, damage, and durability as other
+    */ 
     public Weapon(Weapon other) {
     // TODO: implement
     }
 
 
     // Getters
+    /** Returns the name of the weapon
+     *  Postcondition - Returns the name of type String
+     */
     public String getName() { 
         /* TODO */
     }
+
+    
+    /** Returns the damage value of the weapon
+     *  Postcondition - Returns the damage, of type int
+     */
     public int getDamage() {
         /* TODO */ 
-        }
+    }
+
+    /** Returns the durability value of the weapon
+     *  Postcondition - Returns the durability of type int
+     */
     public int getDurability() { 
         /* TODO */
-        }
+    }
 
 
     // Methods
+    /** Reduces durability by amount
+     * Precondition - amount >= 0
+     *  Postcondition - durability is reduced by amount, minimum 0
     public void weaken(int amount) { 
         /* TODO */ 
-        }
+    }
+
+     /** Checks if weapon is broken
+     *  Returns true if durability <= 0, else false
     public boolean isBroken() {
         /* TODO */
-        }
+    }
 }
